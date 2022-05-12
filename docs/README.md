b8fba5aa698344088c621a6be02af52633556fdc
cqc <tuomodaji0726@163.com>
2022-05-12 17:03:39 +0800

    :wrench: add config

--- docs/README.md
+++ docs/README.md
@@ -9,3 +9,31 @@ description: 页面的描述
 这题 23333
 
 ![](https://gimg3.baidu.com/search/src=http%3A%2F%2Fpics6.baidu.com%2Ffeed%2Fae51f3deb48f8c543565a4756bea60ffe1fe7f07.jpeg%3Ftoken%3D74219a51f9a18499b91c2a877e82dccf&refer=http%3A%2F%2Fwww.baidu.com&app=2021&size=f360,240&n=0&g=0n&q=75&fmt=auto?sec=1652202000&t=a92a8b991a584b65bbad87481a7947f5)
+
+<details>
+<summary>CLICK ME</summary>
+<p>
+
+## We can hide anything, even code!
+
+```mermaid
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+```
+
+| First Header  | Second Header |
+| ------------- | ------------- |
+| Content Cell  | Content Cell  |
+| Content Cell  | Content Cell  |
+
+
+| Command | Description |
+| --- | --- |
+| git status | List all new or modified files |
+| git diff | Show file differences that haven't been staged |
+
+</p>
+</details>
--- vuepress.config.ts
+++ vuepress.config.ts
@@ -1,7 +1,24 @@
-import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite';
+import { defineUserConfig } from '@vuepress/cli';
+const path = require('path');
+
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
-})
+  description: 'hello VuePress',
+  bundler: viteBundler({
+    // https://v2.vuepress.vuejs.org/zh/reference/bundler/vite.html#%E9%85%8D%E7%BD%AE%E9%A1%B9
+    viteOptions: {
+      // dist: path.resolve(__dirname, 'src')
+    },
+    vuePluginOptions: {},
+  }),
+  
+  dest: path.resolve(__dirname, 'distc'),  // build outDir
+  base: '/cqc/', // 如果你的网站部署在非根路径下，即 base 不是 "/" ，你需要把 base 添加到 Public 文件的绝对路径前。
+  public: path.resolve('static'), // static source, 它们会被复制到最终生成的网站的根目录下。
+
+  // devServer
+  port: 3366
+});
